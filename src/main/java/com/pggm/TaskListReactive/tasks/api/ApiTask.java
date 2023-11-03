5b2a79ad72e6e58d20f852dd6ec5e183e0c4ad13
gabriel <pgabrieldeveloper@outlook.com>
2023-11-02 23:25:20 -0300

    criando get task e adicionar task como concluida e adicionando imports

--- src/main/java/com/pggm/TaskListReactive/tasks/api/ApiTask.java
+++ src/main/java/com/pggm/TaskListReactive/tasks/api/ApiTask.java
@@ -2,9 +2,7 @@ package com.pggm.TaskListReactive.tasks.api;
 
 import com.pggm.TaskListReactive.tasks.dtos.CreateTaskDTO;
 import com.pggm.TaskListReactive.tasks.models.Task;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RequestMapping(value = "tasks")
